e4e8fc62cc34e28fabcfe8d0690813ebfd676075
Aswin p b <90848994+17thvoyager@users.noreply.github.com>
2023-09-03 16:28:37 +0530

    commit changes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,55 +1,79 @@
-import React from 'react';
-import { useEffect,useState } from 'react';
-import classes from './AvailableMeals.module.css';
-import Card from '../UI/Card.js';
-import MealItem from '../Meals/MealItem/MealItem.js';
+import React from "react";
+import { useEffect, useState } from "react";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card.js";
+import MealItem from "../Meals/MealItem/MealItem.js";
 
-const AvailableMeals = () => { 
-
+const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
- useEffect(() => {
-  const fetchMeals = async () => {
-    const response = await fetch('https://react-http-8cc39-default-rtdb.firebaseio.com/meals.json');
-    const responseData = await response.json();
+  useEffect(() => {
+    const fetchMeals = async () => {
+        const response = await fetch(
+          "https://react-http-8cc39-default-rtdb.firebaseio.com/meals.json"
+        );
+        
+        if (!response.ok) {
+          throw new Error("Failed to fetch meals.");
+        }
+      
+        const responseData = await response.json();
+        
+        const loadedMeals = [];
 
-    const loadedMeals = [];
-
-    for( const key in responseData ){
-      loadedMeals.push({
-        id: key,
-        name: responseData[key].name,
-        description: responseData[key].description,
-        price: responseData[key].price,
-      })
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+        }
+        
+        setMeals(loadedMeals);
     }
+    
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
 
-    setMeals(loadedMeals);
-  };
+  if(isLoading ) {
+    return(
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
-  fetchMeals();
- }
- 
- , []);
+  if(httpError) {
+    return(
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
-
-  const mealsList = meals.map((meals) => (
+  const mealsList = meals.map((meal) => (
     <MealItem
-      id={meals.id}
-      key={meals.id}
-      name={meals.name}
-      description={meals.description}
-      price={meals.price}
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
     />
   ));
 
-    return (
-      <section className={classes.meals}>
-        <Card>
-          <ul>{mealsList}</ul>
-        </Card>
-      </section>
-    );
-  };
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
 
-  export default AvailableMeals;
+export default AvailableMeals;
